c49602004442c5ebe3b96eb71e67303629f5e579
Miquel Angel Quinones Garcia <m.quinones.garcia@gmail.com>
2013-02-20 19:40:46 +0100

    [mac]Changed build scripts to break dependencies building and release. This can help project newcomers and development too.

---
+++ IndieLib/mac/buildDependencies.sh
@@ -0,0 +1,26 @@
+#bin/sh
+DEPSDIR="../common/dependencies"
+
+#build dependencies
+echo "DEPENDENCIES RELATIVE DIR: $DEPSDIR"
+
+cd $DEPSDIR
+
+#builds static + shared. Will delete shared to link statically
+cd ./FreeImage
+make clean
+make
+rm Dist/*.dylib
+
+#build static SDL lib only
+cd ../SDL-2.0
+./configure --disable-shared --enable-static --prefix=`pwd`/osx
+make clean
+make
+make install
+
+#builds static + shared. Will delete shared to link statically
+cd ../glew-1.9.0
+make clean
+make
+rm lib/*.dylib
